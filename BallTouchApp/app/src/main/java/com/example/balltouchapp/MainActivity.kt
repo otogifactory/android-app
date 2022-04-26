94925b5548f28c440eb95161c1cc7235964bd74f
yk-tsuji <y-tsuji@cainz.co.jp>
2022-04-26 18:59:44 +0900

    up start view

--- BallTouchApp/app/src/main/java/com/example/balltouchapp/MainActivity.kt
+++ BallTouchApp/app/src/main/java/com/example/balltouchapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,17 +12,14 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-        var wm = windowManager.currentWindowMetrics
-
-        // 新しいクラスを表示させる
-        val ballView = BallView(this)
-        ballView.init()
-        ballView.setDisplyaSize(wm.bounds.width(), wm.bounds.height())
-        setContentView(ballView)
-
-//        var iv :ImageView = findViewById(R.id.iv)
-//        iv.setImageResource(R.drawable.cat01)
+        buttonStart.setOnClickListener {
+            var wm = windowManager.currentWindowMetrics
+            val ballView = BallView(this)
+            ballView.init()
+            ballView.setDisplyaSize(wm.bounds.width(), wm.bounds.height())
+            setContentView(ballView)
+        }
     }
 }
